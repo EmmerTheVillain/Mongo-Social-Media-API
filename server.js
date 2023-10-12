6b13e342471f4f6f844fdaa3ea20ba67f339e72d
Mike Barber <bandofgold1@gmail.com>
2023-10-12 11:21:06 -0400

    set up boilerplate for server and mongoose

---
+++ server.js
@@ -0,0 +1,12 @@
+//dependencies
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+//express
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
